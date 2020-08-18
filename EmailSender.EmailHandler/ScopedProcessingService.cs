93da6b293de82d97a08d8318b8ad47862e9fde12
Artem Trusevich <artemt@sweetsoft.com.ua>
2020-08-18 10:32:51 +0300

    fixed random time between sending

--- EmailSender.EmailHandler/ScopedProcessingService.cs
+++ EmailSender.EmailHandler/ScopedProcessingService.cs
@@ -23,57 +23,34 @@ namespace EmailSender.EmailHandler
     {
         private IEmailService _service;
         private IEmailHandlerRepository _repo;
-        //private DbContextOptionsBuilder<ApplicationContext> optionsBuilder;
 
-        public ScopedProcessingService(IEmailService service, IEmailHandlerRepository repository)
+        public ScopedProcessingService(IEmailHandlerRepository repository)
         {
-            _service = service;
             _repo = repository;
         }
 
         public async Task DoWork(CancellationToken stoppingToken)
         {
-            IEnumerable<Email> emails;
-            //var _repo = GetContext();
-            emails = await _repo.GetNewWithTemplates();
-            //_repo.Dispose();
+            var emails = await _repo.GetNewWithTemplates();
             if (emails.Count() > 0)
             {
                 emails.ForAll(x => x.Status = EmailStatus.InProgress);
-                //_repo = GetContext();
-                //_repo.Dispose();
-                try
+                emails.ToList().ForEach(async (email) =>
                 {
-                    // var task = Task.Run(
-                    //delegate
-                    //{
-                    emails.ToList().ForEach(async (email) =>
+                    try
                     {
-                        try
-                        {
-                            await _repo.Update(email);
-                            _service.Send(email.Recipient.Email, email.Template.Subject, email.Template.Body);
-                            email.Status = EmailStatus.Finished;
-                            await _repo.Update(email);
-                        }
-                        catch (Exception ex)
-                        {
-                            email.Status = EmailStatus.New;
-                        }
-                    });
-                    //});
-                }
-                catch (Exception ex)
-                {
-                }
-
+                        await _repo.Update(email);
+                        _service.Send(email.Recipient.Email, email.Template.Subject, email.Template.Body);
+                        email.Status = EmailStatus.Finished;
+                        await _repo.Update(email);
+                        Thread.Sleep(new Random().Next(1000, 40000));
+                    }
+                    catch (Exception ex)
+                    {
+                        email.Status = EmailStatus.New;
+                    }
+                });
             }
         }
-        //private EmailRepository GetContext()
-        //{
-        //    var optionsBuilder = new DbContextOptionsBuilder<ApplicationContext>();
-        //    optionsBuilder.UseSqlServer("Server=SW-DEV88\\SQLEXPRESS;Database=EmailSender;Trusted_Connection=True;MultipleActiveResultSets=True");
-        //    return new EmailRepository(new ApplicationContext(optionsBuilder.Options));
-        //}
     }
 }
